6feec41a139518ed54a271475aa4bd7dd862804a
shigureui2 <tnguyen2@ucvts.org>
2024-01-29 17:43:45 -0500

    Everything in GroundIntake.java is done, one method unsure of

--- src/main/java/frc/robot/subsystems/groundIntake/GroundIntake.java
+++ src/main/java/frc/robot/subsystems/groundIntake/GroundIntake.java
@@ -1,8 +1,11 @@
 package frc.robot.subsystems.groundIntake;
 
 import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
+import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import static frc.robot.Constants.GroundIntake.*;
 import static frc.robot.Constants.GroundIntake.GROUND_INTAKE_PID;
 
@@ -18,9 +21,21 @@ public class GroundIntake extends SubsystemBase {
     
     public GroundIntake (GroundIntakeIO io) {
         this.io = io;
+        SmartDashboard.putData(getName(), this);
     }
+
     public void periodic() {
-
+        io.updateInputs(inputs);
+        Logger.processInputs("Ground Intake", inputs);
     }
-
+    public void setVoltage(double voltage) {
+        io.setVoltage(voltage);
+    }
+    public void breakBeamSensor() {
+        // io.breakBeamSensor(); Not sure how to implement this method
+        SmartDashboard.putBoolean("Ground Intake Break Beam", breakBeamSensor.get());
+    }
+    public void setBrake(boolean brake) {
+        io.setBrake(brake);
+    }
 }
