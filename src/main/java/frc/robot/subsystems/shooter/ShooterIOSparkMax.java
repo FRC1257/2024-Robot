384e3b25195949db9f76d98ea32aaeb8db66eccc
clairethebear1 <153558931+clairethebear1@users.noreply.github.com>
2024-02-05 16:13:01 -0500

    Update ShooterIOSparkMax.java - copied mechanical advantage

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkMax.java
@@ -1,36 +1,169 @@
-//hello
-package frc.robot.subsystems.drive;
+package frc.robot.subsystems.superstructure.shooter;
 
-public class ShooterIOSparkMax implements ShooterIO{
-    private CANSparkMax shooterMotor;
-    private RelativeEncoder encoder;
-    private DutyCycleEncoder absoluteEncoder;
+import static frc.robot.subsystems.superstructure.SuperstructureConstants.ShooterConstants.*;
 
-    public ShooterIOSparkMax() {
-        shooterMotor = new CANSparkMax(SHOOTER_MOTOR_ID, MotorType.kBrushless);
-        shooterMotor.restoreFactoryDefaults();
-        shooterMotor.setIdleMode(IdleMode.kBrake);
-        shooterMotor.setSmartCurrentLimit(NEO_CURRENT_LIMIT);
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkFlex;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 
-        encoder = shooterMotor.getEncoder();
-        encoder.setPositionConversionFactor(SHOOTER_REV_TO_POS_FACTOR);
-        encoder.setVelocityConversionFactor(SHOOTER_REV_TO_POS_FACTOR / 60);
-        encoder.setPosition(0.6);
+public class ShooterIOSparkMax implements ShooterIO {
+  private CANSparkFlex leftMotor;
+  private CANSparkFlex rightMotor;
+  private CANSparkFlex feederMotor;
 
-        absoluteEncoder = new DutyCycleEncoder(0);
-        absoluteEncoder.setDistancePerRotation(360.0 / 1024.0);
-        absoluteEncoder.setDutyCycleRange(1 / 1024.0, 1023.0 / 1024.0);
+  private RelativeEncoder leftEncoder;
+  private RelativeEncoder rightEncoder;
 
-        encoder.setPosition(absoluteEncoder.getDistance() * 28.45 + 0.6);
-    }
+  private SparkPIDController leftController;
+  private SparkPIDController rightController;
+  private SimpleMotorFeedforward leftFF = new SimpleMotorFeedforward(0.0, 0.0, 0.0);
+  private SimpleMotorFeedforward rightFF = new SimpleMotorFeedforward(0.0, 0.0, 0.0);
 
-     /** Updates the set of loggable inputs. */
-    @Override
-    public void updateInputs(ElevatorIOInputs inputs) {
-        inputs.positionMeters = encoder.getPosition();
-        inputs.velocityMeters = encoder.getVelocity();
-        inputs.appliedVolts = shooterMotor.getAppliedOutput() * shooterMotor.getBusVoltage();
-        inputs.currentAmps = new double[] {shooterMotor.getOutputCurrent()};
-        inputs.tempCelsius = new double[] {shooterMotor.getMotorTemperature()};
-    }
+  public ShooterIOSparkMax() {
+    leftMotor = new CANSparkFlex(leftFlywheelConstants.id(), CANSparkFlex.MotorType.kBrushless);
+    rightMotor = new CANSparkFlex(rightFlywheelConstants.id(), CANSparkFlex.MotorType.kBrushless);
+    feederMotor = new CANSparkFlex(feederConstants.id(), CANSparkFlex.MotorType.kBrushless);
+
+    leftEncoder = leftMotor.getEncoder();
+    rightEncoder = rightMotor.getEncoder();
+
+    leftMotor.restoreFactoryDefaults();
+    rightMotor.restoreFactoryDefaults();
+    feederMotor.restoreFactoryDefaults();
+
+    leftMotor.setInverted(leftFlywheelConstants.inverted());
+    rightMotor.setInverted(rightFlywheelConstants.inverted());
+    leftMotor.setSmartCurrentLimit(60);
+    rightMotor.setSmartCurrentLimit(60);
+    leftMotor.enableVoltageCompensation(12.0);
+    rightMotor.enableVoltageCompensation(12.0);
+
+    feederMotor.setInverted(feederConstants.inverted());
+    feederMotor.setSmartCurrentLimit(100);
+    feederMotor.enableVoltageCompensation(12.0);
+
+    setShooterBrakeMode(false);
+    setFeederBrakeMode(false);
+
+    leftEncoder.setPosition(0.0);
+    rightEncoder.setPosition(0.0);
+    //    leftEncoder.setMeasurementPeriod(10);
+    //    rightEncoder.setMeasurementPeriod(10);
+    //    leftEncoder.setAverageDepth(2);
+    //    rightEncoder.setAverageDepth(2);
+
+    // rotations, rps
+    leftEncoder.setPositionConversionFactor(1.0 / flywheelReduction);
+    rightEncoder.setPositionConversionFactor(1.0 / flywheelReduction);
+    leftEncoder.setVelocityConversionFactor(1.0 / flywheelReduction);
+    rightEncoder.setVelocityConversionFactor(1.0 / flywheelReduction);
+
+    leftController = leftMotor.getPIDController();
+    rightController = rightMotor.getPIDController();
+    leftController.setFeedbackDevice(leftEncoder);
+    rightController.setFeedbackDevice(rightEncoder);
+
+    leftMotor.burnFlash();
+    rightMotor.burnFlash();
+    feederMotor.burnFlash();
+  }
+
+  @Override
+  public void updateInputs(ShooterIOInputs inputs) {
+    inputs.leftFlywheelPositionRotations = leftEncoder.getPosition();
+    inputs.leftFlywheelVelocityRPM = leftEncoder.getVelocity();
+    inputs.leftFlywheelAppliedVolts = leftMotor.getAppliedOutput();
+    inputs.leftFlywheelOutputCurrent = leftMotor.getOutputCurrent();
+
+    inputs.rightFlywheelPositionRotations = rightEncoder.getPosition();
+    inputs.rightFlywheelVelocityRPM = rightEncoder.getVelocity();
+    inputs.rightFlywheelAppliedVolts = rightMotor.getAppliedOutput();
+    inputs.rightFlywheelOutputCurrent = rightMotor.getOutputCurrent();
+
+    inputs.feederVelocityRPM = feederMotor.getEncoder().getVelocity();
+    inputs.feederAppliedVolts = feederMotor.getAppliedOutput();
+    inputs.feederOutputCurrent = feederMotor.getOutputCurrent();
+  }
+
+  @Override
+  public void setLeftRPM(double rpm) {
+    leftController.setReference(
+        rpm,
+        CANSparkBase.ControlType.kVelocity,
+        0,
+        leftFF.calculate(rpm),
+        SparkPIDController.ArbFFUnits.kVoltage);
+  }
+
+  @Override
+  public void setRightRPM(double rpm) {
+    rightController.setReference(
+        rpm,
+        CANSparkBase.ControlType.kVelocity,
+        0,
+        rightFF.calculate(rpm),
+        SparkPIDController.ArbFFUnits.kVoltage);
+  }
+
+  @Override
+  public void setFeederVoltage(double volts) {
+    feederMotor.setVoltage(volts);
+  }
+
+  @Override
+  public void setLeftCharacterizationVoltage(double volts) {
+    leftMotor.setVoltage(volts);
+  }
+
+  @Override
+  public void setRightCharacterizationVoltage(double volts) {
+    rightMotor.setVoltage(volts);
+  }
+
+  @Override
+  public void setLeftBrakeMode(boolean enabled) {
+    leftMotor.setIdleMode(enabled ? CANSparkBase.IdleMode.kBrake : CANSparkBase.IdleMode.kCoast);
+  }
+
+  @Override
+  public void setRightBrakeMode(boolean enabled) {
+    rightMotor.setIdleMode(enabled ? CANSparkBase.IdleMode.kBrake : CANSparkBase.IdleMode.kCoast);
+  }
+
+  @Override
+  public void setFeederBrakeMode(boolean enabled) {
+    feederMotor.setIdleMode(enabled ? CANSparkBase.IdleMode.kBrake : CANSparkBase.IdleMode.kCoast);
+  }
+
+  @Override
+  public void setLeftPID(double p, double i, double d) {
+    leftController.setP(p);
+    leftController.setI(i);
+    leftController.setD(d);
+  }
+
+  @Override
+  public void setLeftFF(double kS, double kV, double kA) {
+    leftFF = new SimpleMotorFeedforward(kS, kV, kA);
+  }
+
+  @Override
+  public void setRightPID(double p, double i, double d) {
+    rightController.setP(p);
+    rightController.setI(i);
+    rightController.setD(d);
+  }
+
+  @Override
+  public void setRightFF(double s, double v, double a) {
+    rightFF = new SimpleMotorFeedforward(s, v, a);
+  }
+
+  @Override
+  public void stop() {
+    setRPM(0.0, 0.0);
+    setFeederVoltage(0.0);
+  }
 }
