b65f74e9f3f736c7b6497cbcabb4272a818ff8fc
shigureui2 <152223801+shigureui2@users.noreply.github.com>
2024-02-05 16:37:40 -0500

    add PID to intakeIO

--- src/main/java/frc/robot/subsystems/Intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIO.java
@@ -17,8 +17,25 @@ public interface IntakeIO {
     public default void updateInputs(IntakeIOInputs inputs) {}
     /** sets voltage to run motor if necessary */
     public default void setVoltage(double voltage) {}
+    public default void setPIDConstants(double p, double i, double d, double ff) {}
     /** sets brake mode */
     public default void setBrake(boolean brake) {}
 
     public default boolean isIntaked() {return true;}
+
+    public default void setP(double p) {}
+    
+    public default void setI(double i) {}
+
+    public default void setD(double d) {}
+
+    public default void setFF(double ff) {}
+
+    public default double getP() { return 0.0; }
+
+    public default double getI() { return 0.0; }
+
+    public default double getD() { return 0.0; }
+
+    public default double getFF() { return 0.0; }
 }
