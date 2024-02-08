b4b18a054bc9d2ef3dd958fee152fb3bbaac0fcc
Caitlin-78 <148150215+Caitlin-78@users.noreply.github.com>
2024-02-08 21:11:15 +0000

    Made ground intake

---
+++ src/main/java/frc/robot/subsystems/Ground Intake/GroundIntake.java
@@ -0,0 +1,109 @@
+package frc.robot.subsystems.GroundIntake;
+
+import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.GroundIntake.*;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+
+public class GroundIntake extends SubsystemBase {
+    private final GroundIntakeIO io;
+    GroundIntakeIOInputsAutoLogged inputs = new GroundIntakeIOInputsAutoLogged();
+    
+    private LoggedDashboardNumber logP;
+    private LoggedDashboardNumber logI;
+    private LoggedDashboardNumber logD;
+    
+    public GroundIntake (GroundIntakeIO io) {
+        this.io = io;
+        SmartDashboard.putData(getName(), this);
+
+        logP = new LoggedDashboardNumber("GroundIntake/P", io.getP());
+        logI = new LoggedDashboardNumber("GroundIntake/I", io.getI());
+        logD = new LoggedDashboardNumber("GroundIntake/D", io.getD());
+    }
+
+    public void periodic() {
+        io.updateInputs(inputs);
+        // Update PID constants to ensure they are up to date
+        if(logP.get() != io.getP()) {
+            io.setP(logP.get());
+        }
+        if(logI.get() != io.getI()) {
+            io.setI(logI.get());
+        }
+        if(logD.get() != io.getD()) {
+            io.setD(logD.get());
+        }
+        Logger.processInputs("GroundIntake", inputs);
+    }
+
+    public void setVoltage(double voltage) {
+        io.setVoltage(voltage);
+    }
+
+    public void setBrake(boolean brake) {
+        io.setBrake(brake);
+    }
+
+    public boolean isIntaked(){
+        return io.isIntaked();
+    }
+
+    //replace with whatever you want
+    // Dependence on pivot angle and shooter break beam
+    public Command GroundIntakeLoopCommand(double voltage) {
+        return new FunctionalCommand(
+            () -> {},
+            () -> setVoltage(voltage),
+            (stop) -> setVoltage(0.0),
+            this::isIntaked,
+            this
+        );
+    }
+
+    /**
+     * Same dependence as GroundIntakeLoopCommand
+     */ 
+    public Command EjectLoopCommand(double voltage) {
+        return new FunctionalCommand(
+            () -> {},
+            () -> setVoltage(-voltage), // Spins the other way
+            (stop) -> setVoltage(0.0),
+            this::isIntaked,
+            this
+        );
+    }
+
+    public Command GroundIntakeSpeedCommand(DoubleSupplier speed) {
+        return new FunctionalCommand(
+            () -> {},
+            () -> io.setSpeed(speed.getAsDouble()),
+            (stop) -> io.stop(),
+            () -> false,
+            this
+        );
+    }
+
+    public Command GroundIntakeManualCommand(DoubleSupplier voltage) {
+        return new FunctionalCommand(
+            () -> {},
+            () -> io.setVoltage(voltage.getAsDouble()),
+            (stop) -> io.stop(),
+            () -> false,
+            this
+        );
+    }
+
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/Ground Intake/GroundIntakeIOSparkMax.java
@@ -0,0 +1,112 @@
+package frc.robot.subsystems.GroundIntake;
+
+import static frc.robot.Constants.NEO_CURRENT_LIMIT;
+import static frc.robot.Constants.GroundIntake.GroundIntakeSimConstants.*;
+
+import com.revrobotics.CANSparkBase.ControlType;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.Constants.ElectricalLayout;
+
+/** Need to import Constants files/classes */
+// 
+
+public class GroundIntakeIOSparkMax implements GroundIntakeIO {
+
+    private CANSparkMax GroundIntakeMotor;
+    private RelativeEncoder GroundintakeEncoder;
+    private SparkPIDController velocityPID;
+
+    private double desiredSpeed;
+
+    public GroundIntakeIOSparkMax() {
+        /** ID needs to be assigned from constants */
+        setPIDConstants(kGroundIntakeP, kGroundIntakeI, kGroundIntakeD);
+        GroundIntakeMotor = new CANSparkMax(ElectricalLayout.GROUND_INTAKE_MOTOR, CANSparkMax.MotorType.kBrushless);
+        GroundIntakeMotor.restoreFactoryDefaults();
+        GroundIntakeMotor.setIdleMode(IdleMode.kBrake);
+        /** Current limit should be added to Constants.java when known */
+        GroundIntakeMotor.setSmartCurrentLimit(NEO_CURRENT_LIMIT);
+
+        GroundintakeEncoder = GroundIntakeMotor.getEncoder();
+
+
+        velocityPID = GroundIntakeMotor.getPIDController();
+    }
+
+    /** updates inputs from robot */
+    @Override
+    public void updateInputs(GroundIntakeIOInputs inputs) {
+        inputs.appliedVoltage = GroundIntakeMotor.getAppliedOutput() * GroundIntakeMotor.getBusVoltage();
+        inputs.currentAmps = new double[] { GroundIntakeMotor.getOutputCurrent() };
+        inputs.tempCelcius = new double[] { GroundIntakeMotor.getMotorTemperature() };
+        inputs.velocityRadsPerSec = GroundintakeEncoder.getVelocity();
+        inputs.speedSetpoint = desiredSpeed;
+        inputs.breakBeam = breakBeam.get();
+    }
+
+    /** sets voltage to run motor if necessary */
+    @Override
+    public void setVoltage(double voltage) {
+        GroundIntakeMotor.setVoltage(voltage);
+    }
+
+    /** sets brake mode to stop */
+    @Override
+    public void setBrake(boolean brake) {
+        GroundIntakeMotor.setIdleMode(brake ? IdleMode.kBrake : IdleMode.kCoast);
+    }
+
+
+    @Override
+    public void setSpeed(double speed) {
+        desiredSpeed = speed;
+        velocityPID.setReference(speed, ControlType.kVelocity);
+    }
+
+    @Override
+    public void setP(double p) {
+        velocityPID.setP(p);
+    }
+
+    @Override
+    public void setI(double i) {
+        velocityPID.setI(i);
+    }
+
+    @Override
+    public void setD(double d) {
+        velocityPID.setD(d);
+    }
+
+    @Override
+    public double getP() {
+        return velocityPID.getP();
+    }
+
+    @Override
+    public double getI() {
+        return velocityPID.getI();
+    }
+
+    @Override
+    public double getD() {
+        return velocityPID.getD();
+    }
+
+    @Override
+    public void stop() {
+        setVoltage(0.0);
+    }
+
+    @Override
+    public void setPIDConstants(double p, double i, double d) {
+        velocityPID.setP(p);
+        velocityPID.setI(i);
+        velocityPID.setD(d);
+    }
+}
