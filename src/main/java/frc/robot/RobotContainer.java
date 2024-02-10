cfbf095436e5cfec04a5166d7f81acabb4186174
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2024-02-10 11:56:35 -0500

    add note visualization

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -60,6 +60,7 @@ import frc.robot.subsystems.drive.ModuleIOSparkMax;
 import frc.robot.subsystems.intake.*;
 import frc.robot.subsystems.vision.*;
 import frc.robot.util.CommandSnailController;
+import frc.robot.util.note.NoteVisualizer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
@@ -236,6 +237,9 @@ public class RobotContainer {
 
     operator.getB().onTrue(pivot.PIDCommand(Constants.PivotArm.PIVOT_ARM_MAX_ANGLE));
     operator.getX().onTrue(pivot.PIDCommand(Constants.PivotArm.PIVOT_ARM_MIN_ANGLE));
+
+    NoteVisualizer.setRobotPoseSupplier(drive::getPose, () -> 5.0, () -> 6.0, pivot::getAngle);
+    driver.getA().onTrue(NoteVisualizer.shoot());
 
 
     /* intake.setDefaultCommand(
---
+++ src/main/java/frc/robot/util/note/NoteShot.java
@@ -0,0 +1,90 @@
+package frc.robot.util.note;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.geometry.Twist3d;
+
+import static frc.robot.Constants.PivotArm.PivotArmSimConstants.kArmLength;
+
+import org.littletonrobotics.junction.Logger;
+
+public class NoteShot {
+    private Pose3d shotPosition;
+    private double shotStraightSpeed;
+    private double shotTangentSpeed;
+
+    public NoteShot(Pose3d shotPosition, double shotStraightSpeed, double shotTangentSpeed) {
+        this.shotPosition = shotPosition;
+        this.shotStraightSpeed = shotStraightSpeed;
+        this.shotTangentSpeed = shotTangentSpeed;
+    }
+
+    public Pose3d getShotPosition() {
+        return shotPosition;
+    }
+
+    public double getShotStraightSpeed() {
+        return shotStraightSpeed;
+    }
+
+    public double getShotTangentSpeed() {
+        return shotTangentSpeed;
+    }
+
+    public void setShotPosition(Pose3d shotPosition) {
+        this.shotPosition = shotPosition;
+    }
+
+    public void setShotStraightSpeed(double shotStraightSpeed) {
+        this.shotStraightSpeed = shotStraightSpeed;
+    }
+
+    public void setShotTangentSpeed(double shotTangentSpeed) {
+        this.shotTangentSpeed = shotTangentSpeed;
+    }
+
+    public static NoteShot fromRobotInfo(Pose2d robotPose, Rotation2d pivotAngle, double shotLeftSpeed, double shotRightSpeed) {
+        // calculate the position of the arm in 2d
+        Pose2d armPose = robotPose.plus(new Transform2d(new Translation2d(0.098, Rotation2d.fromDegrees(180)), new Rotation2d()));
+        
+        // get the 3d rotation of the arm
+        Rotation3d rotation = new Rotation3d(0, pivotAngle.getRadians(), armPose.getRotation().plus(Rotation2d.fromDegrees(180)).getRadians());
+        Rotation3d noteRotation = new Rotation3d(0, Math.PI / 2, 0);
+        
+        Translation3d translation = new Translation3d(armPose.getTranslation().getX(), armPose.getTranslation().getY(), 0.28);
+        Pose3d extendedPose = new Pose3d(translation, rotation);
+
+        extendedPose = extendedPose.plus(new Transform3d(new Translation3d(-kArmLength, 0, 0), noteRotation));
+
+        Logger.recordOutput("ExtendedPOse", extendedPose);
+
+        double straightSpeed = (shotLeftSpeed + shotRightSpeed) / 2;
+        double tangentSpeed = (shotLeftSpeed - shotRightSpeed) / 2;
+
+        return new NoteShot(extendedPose, straightSpeed, tangentSpeed);
+    }
+
+    public PathPoint getFirstPoint() {
+        // fix this code
+        double dx = shotStraightSpeed * Math.cos(shotPosition.getRotation().getZ() + Math.PI)
+            /* + shotTangentSpeed * Math.cos(shotPosition.getRotation().getZ()) */;
+        double dy = shotStraightSpeed * Math.sin(shotPosition.getRotation().getZ()) 
+            /* + shotTangentSpeed * Math.sin(shotPosition.getRotation().getZ() + Math.PI / 2) */;
+        double dz = shotStraightSpeed * Math.sin(shotPosition.getRotation().getY());
+        
+        return new PathPoint(
+            shotPosition, 
+            dx,
+            dy,
+            dz, 
+            0
+        );
+    }
+
+}
