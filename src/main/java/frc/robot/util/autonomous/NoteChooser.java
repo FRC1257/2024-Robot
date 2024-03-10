c014de179c89ac3676845fe6af0e5f0bd1a33763
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2024-03-10 06:46:23 -0400

    add note sendable

--- src/main/java/frc/robot/util/autonomous/NoteChooser.java
+++ src/main/java/frc/robot/util/autonomous/NoteChooser.java
@@ -3,6 +3,7 @@ package frc.robot.util.autonomous;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.FieldConstants;
 
 public class NoteChooser {
@@ -11,6 +12,7 @@ public class NoteChooser {
 
     public NoteChooser(String name){
         position = new SendableChooser<Pose2d>();
+        SmartDashboard.putData(name, position);
     }
 
     public void setNotePosition(){
