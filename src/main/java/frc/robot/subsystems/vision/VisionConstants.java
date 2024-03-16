4d8d36255f9b2e5a070657b07284d9f01cf287a4
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2024-03-16 13:29:55 -0400

    fix offset

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -13,55 +13,64 @@ import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 
 public class VisionConstants {
-    public static final String kRaspberryCameraName = "April_Camera";
-    public static final String kOrangeCameraName = "";
-    public static final String kNoteCameraName = "Note_Camera";
-    public static final String kRaspberryCameraName2 = "April_Camera2";
-    // Cam mounted facing forward, half a meter forward of center, half a meter up
-    // from center.
-    private static double cameraPitchAngle = Rotation2d.fromDegrees(61).getRadians();
-    public static final Transform3d kRaspberryRobotToCam = new Transform3d(
-        new Translation3d(
-                Units.inchesToMeters(10.75), 
-                Units.inchesToMeters(-11), 
-                Units.inchesToMeters(9)
-        ),
-            new Rotation3d(0, cameraPitchAngle, Rotation2d.fromDegrees(180).getRadians()));
+        public static final String kRaspberryCameraName = "April_Camera";
+        public static final String kOrangeCameraName = "";
+        public static final String kNoteCameraName = "Note_Camera";
+        public static final String kRaspberryCameraName2 = "April_Camera2";
+        // Cam mounted facing forward, half a meter forward of center, half a meter up
+        // from center.
+        private static double cameraPitchAngle = Rotation2d.fromDegrees(61).getRadians();
+        public static final Transform3d kRaspberryRobotToCam = new Transform3d(
+                        new Translation3d(
+                                        Units.inchesToMeters(10.75),
+                                        Units.inchesToMeters(-11),
+                                        Units.inchesToMeters(9)),
+                        new Rotation3d(0, cameraPitchAngle, Rotation2d.fromDegrees(180).getRadians()));
 
+        public static final Transform3d kOrangeRobotToCam = new Transform3d(
+                        new Translation3d(
+                                Units.inchesToMeters(-9.5),
+                                Units.inchesToMeters(7),
+                                Units.inchesToMeters(4)
+                        ),
+                        new Rotation3d(0, 
+                                Rotation2d.fromDegrees(15).getRadians(),
+                                Rotation2d.fromDegrees(180 + 5).getRadians())
+                        );
+        public static final Transform3d kRaspberryRobotToCam2 = new Transform3d(
+                        new Translation3d(
+                                Units.inchesToMeters(-9.5),
+                                Units.inchesToMeters(7),
+                                Units.inchesToMeters(4)
+                        ),
+                        new Rotation3d(0, 
+                                Rotation2d.fromDegrees(15).getRadians(),
+                                Rotation2d.fromDegrees(180 + 5).getRadians())
+                        );
+        public static final Transform3d kNoteCameraNameToCam = new Transform3d(new Translation3d(-0.5, -0.45, 0.23),
+                        new Rotation3d(0, 0, Rotation2d.fromDegrees(155).getRadians()));
+        // Change CameraNameToCam angle and stuff as needed, ask MAURICIO SANCHEZ
 
-    public static final Transform3d kOrangeRobotToCam = new Transform3d(
-        new Translation3d(
-                Units.inchesToMeters(-9.5),
-                Units.inchesToMeters(7),
-                Units.inchesToMeters(4)
-        ),
-            new Rotation3d(0, Rotation2d.fromDegrees(15).getRadians(), Rotation2d.fromDegrees(180+5).getRadians()));
-    public static final Transform3d kRaspberryRobotToCam2 = new Transform3d(new Translation3d(0.5, -0.45, 0.23),
-            new Rotation3d(0, 0, Rotation2d.fromDegrees(155).getRadians()));
-    public static final Transform3d kNoteCameraNameToCam = new Transform3d(new Translation3d(-0.5, -0.45, 0.23),
-            new Rotation3d(0, 0, Rotation2d.fromDegrees(155).getRadians()));
-    // Change CameraNameToCam angle and stuff as needed, ask MAURICIO SANCHEZ
+        public static final int NOTE_PIPELINE = 0;
+        public static final int NOTE_TAG_PIPELINE = 1;
 
-    public static final int NOTE_PIPELINE = 0;
-    public static final int NOTE_TAG_PIPELINE = 1;
+        public static final double NoteCameraHeight = 6.0;
 
-    public static final double NoteCameraHeight = 6.0;
+        public static final double NoteHeight = 0.0;
 
-    public static final double NoteHeight = 0.0;
+        // Note Camera Angle (used in sim)
+        public static final Transform3d kNoteRobotToCam = new Transform3d(new Translation3d(0.5, 0.45, 0.23),
+                        new Rotation3d(0, 0, 0));
 
-    // Note Camera Angle (used in sim)
-    public static final Transform3d kNoteRobotToCam = new Transform3d(new Translation3d(0.5, 0.45, 0.23),
-            new Rotation3d(0, 0, 0));
+        // The layout of the AprilTags on the field
+        public static final AprilTagFieldLayout kTagLayout = AprilTagFields.kDefaultField.loadAprilTagLayoutField();
 
-    // The layout of the AprilTags on the field
-    public static final AprilTagFieldLayout kTagLayout = AprilTagFields.kDefaultField.loadAprilTagLayoutField();
+        public static final double AMBIGUITY_THRESHOLD = 0.5;
+        public static final double MAX_DISTANCE = 4; // meters
 
-    public static final double AMBIGUITY_THRESHOLD = 0.5;
-    public static final double MAX_DISTANCE = 4; // meters
-
-    // The standard deviations of our vision estimated poses, which affect
-    // correction rate
-    // (Fake values. Experiment and determine estimation noise on an actual robot.)
-    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
-    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
+        // The standard deviations of our vision estimated poses, which affect
+        // correction rate
+        // (Fake values. Experiment and determine estimation noise on an actual robot.)
+        public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(4, 4, 8);
+        public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1);
 }
