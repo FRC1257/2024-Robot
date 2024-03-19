113d98c83fc34bb50d7670207fe33525e2fc5bcf
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2024-03-18 20:38:22 -0400

    add unit test to robot code

---
+++ src/test/java/RobotContainerTest.java
@@ -0,0 +1,18 @@
+// Copyright (c) 2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+import frc.robot.RobotContainer;
+import org.junit.jupiter.api.Test;
+
+public class RobotContainerTest {
+
+  @Test
+  public void createRobotContainer() {
+    // Instantiate RobotContainer
+    new RobotContainer();
+  }
+}
