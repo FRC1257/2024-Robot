9db1a533244dbe1b94b6115430a7ce5e99d67c96
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2024-04-19 17:55:16 -0400

    Added additional code to testing class

--- src/test/java/RobotContainerTest.java
+++ src/test/java/RobotContainerTest.java
@@ -4,15 +4,22 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file at
 // the root directory of this project.
-
+ 
 import frc.robot.RobotContainer;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
 public class RobotContainerTest {
 
   @Test
   public void createRobotContainer() {
-    // Instantiate RobotContainer
-    new RobotContainer();
+    try {
+      new RobotContainer();
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Failed to instantiate robot container - see the above stack trace for more information");
+    }
   }
 }
